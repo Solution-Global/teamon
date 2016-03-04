e4c0e319a66b0418b9e571a2a0cc984d14ac669a
antonio91kim <antonio91@gmail.com>
2016-03-04 17:23:45 +0900

    set app root path

--- web/server.js
+++ web/server.js
@@ -4,19 +4,21 @@ var serveStatic = require('serve-static');
 var cpl = require('connect-proxy-layer');
 var logger = require('morgan');
 var fs = require('fs');
+var appRootPath = require('app-root-path');
 
 const tlsOptions = {
   key: fs.readFileSync(__dirname + '/teamon_key.pem'),
   cert: fs.readFileSync(__dirname + '/teamon_cert.pem')
 };
 
+console.log(appRootPath);
 // TODO
 // log to file rotate
 var logFormat = ':remote-addr - [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time';
 var app = connect()
-  .use("/rest", cpl('http://192.168.1.164:7587/'))
+  .use("/rest", cpl('http://192.168.1.164:7587/rest/'))
   .use(logger(logFormat))
-  .use(serveStatic(__dirname));
+  .use(serveStatic(appRootPath.path));
 
 https.createServer(tlsOptions, app).listen(8082, function() {
   console.log('server running on 8082');
