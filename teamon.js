f0b810c0a804b794738abea3d102b3840bf2042e
jingjingea <jingjingea@uangel.com>
2016-03-16 11:01:51 +0900

    toastr  경로 수정 & rest api URL 수정

--- teamon.js
+++ teamon.js
@@ -12,14 +12,14 @@ preferenceManager = require('./script/storage/preference'); // global var
 messageManager = require('./script/storage/message.js'); // global var
 chatModule = require('./script/chat_client.js'); // global var
 cacheManager = require('./script/uCache'); // global var
-
+toastr = require("./script/plugins/toastr/toastr.min.js"); // global var
 timezone = "Asia/Seoul";
 
 function initialize(){
   if(window && window.process && window.process.type) {
     // For desktop
     runningChannel = constants.CHANNEL_APP;
-    aplUrl = "https://192.168.1.164:7587/rest";
+    aplUrl = "http://192.168.1.164:7587/rest/";
   } else {
     // For WEB
     console.trace(navigator.userAgent);
@@ -43,6 +43,8 @@ function initialize(){
   // $(window).resize(function() {
   //   resizeSection();
   // });
+
+  console.log(toastr);
 }
 
 // function reloadInit()
