ed7d7376744eb07e2c50c827a384e17f0a8c58b1
Jerry Jeong <jerrian@gmail.com>
2016-03-21 13:14:26 +0900

    작업중

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        jshint: {
+        'jshint': {
           all: ['Gruntfile.js', 'script/*.js']
         },
+        'electron-installer-windows': {
+          win64: {
+            src: './release/win/teamon-win32-x64',
+            dest: './release/win/installer'
+          }
+        }
       });
 
       grunt.loadNpmTasks('grunt-contrib-jshint');
+      grunt.loadNpmTasks('grunt-electron-installer-windows');
 
       grunt.registerTask('default', ['jshint:all']);
     };
