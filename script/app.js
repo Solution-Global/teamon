9ae2280e3f464f67ee45075964e9c3e842e39821
antonio91kim <antonio91@gmail.com>
2016-03-25 17:51:04 +0900

    to use root path on html for desktop app

--- script/app.js
+++ script/app.js
@@ -1,7 +1,9 @@
+require('app-module-path').addPath(__dirname);
+
 var app = require('app');
 var BrowserWindow = require('browser-window');
 var path = require('path');
-var constants = require('./constants');
+var constants = require('../script/constants');
 
 var INDEX = 'file://' + path.join(__dirname, '../index_app.html');
 
@@ -19,13 +21,29 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   var mainWindow = new BrowserWindow({
-    width: 1400,
+    width: 950,
     height: 700,
     title: constants.APP_NAME
   });
   mainWindow.loadURL(INDEX);
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  var protocol = require('protocol');
+  var fs = require('fs');
+  var appRootPath = require('app-root-path').path.replace(/\\/gi, "/");
+  protocol.interceptFileProtocol('file', function(request, callback) {
+    var url = request.url.substr(7);
+    // console.log(request.url);
+    if (url.lastIndexOf("?") > 0)
+    url = url.substr(0, url.lastIndexOf("?"));
+    url = url.indexOf(appRootPath) == -1 ? appRootPath + url.substr(3) : url.substr(1);
+    // console.log(path.normalize(url));
+    callback(path.normalize(url));
+  }, function (error) {
+    if (error)
+      console.log(error);
+  });
 
   mainWindow.on('closed', function() {
     mainWindow = null;
